8af6d5bfd3584af7218add70091c5f8aa334eb89
gopal265 <84660174+gopal265@users.noreply.github.com>
2021-06-18 07:36:48 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,78 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+int max(int arr[],int count)
+{
+  int maxx=0;
+  for(int i=0;i<count;i++)
+  {
+    if(arr[i] > maxx)
+    {
+      maxx = arr[i];
+    }
+  }
+  return maxx;
+}
+
+int min(int arr[],int count)
+{
+  int minn=arr[0];
+  for(int i=1;i<count;i++)
+  {
+    if(arr[i] < minn)
+    {
+      minn = arr[i];
+    }
+  }
+  return minn;
+}
+
+float average(int arr[],int count)
+{
+  int sum=0;
+  float avg=0.0;
+  for(int i=0;i<count;i++)
+  {
+     sum += arr[i];
+  }
+  avg = sum/count;
+  return avg;
+}
+
+int mode(int arr[],int n)
+{
+  int count=0,maxCount=0,mode=0;
+  for(int i=0; i<n; i++)
+  {
+    for(int j=0; j<n; j++)
+    {
+      
+      if(arr[j] == arr[i])
+      {
+        count +=1;
+      }
+    }
+      if(count>maxCount)
+      {
+      maxCount = count;
+      mode= arr[i];
+      }
+  }
+  return mode; 
+}
+
+int factors(int num,int arr[])
+{
+  int c=0;
+  int k=0;
+  for(int i=2;num>1;i++)
+  {
+    while(num%i==0)
+    {
+      c +=1;
+      arr[k]=i;
+      num = num/i;
+      k++;
+    }
+  }
+  return c;
+}
